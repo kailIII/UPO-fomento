f08ddf8a9ec6afc7f01b0399d00e5e9008cbc77b
Javier Aragón <javier.aragon@geographica.gs>
2015-05-21 09:50:32 +0200

    desplegable familias

--- api/model/FamilyModel.py
+++ api/model/FamilyModel.py
@@ -12,9 +12,11 @@ class FamilyModel(PostgreSQLModel):
     
     
     def getCartoTemListByFamyly(self):
-        sql = "SELECT f.cod_familia, name_familia, cod_indicador, name_indicador, description, (select count(*) from geoserver.indicador_fecha fecha where cod_indicador = i.cod_indicador) as count, (select fecha from geoserver.indicador_fecha fecha where cod_indicador = i.cod_indicador order by fecha limit 1) as fecha, (select count(*) from geoserver.indicador_grafica grafica where grafica.cod_indicador = i.cod_indicador) as graficas" \
+        sql = "SELECT f.cod_familia, f.name_familia, p.cod_familia as fatherCod, p.name_familia as fatherName, cod_indicador, name_indicador, description, (select count(*) from geoserver.indicador_fecha fecha where cod_indicador = i.cod_indicador) as count, (select fecha from geoserver.indicador_fecha fecha where cod_indicador = i.cod_indicador order by fecha limit 1) as fecha, (select count(*) from geoserver.indicador_grafica grafica where grafica.cod_indicador = i.cod_indicador) as graficas" \
                 " from geoserver.familia f" \
-                 " inner join geoserver.indicador i on i.cod_familia = f.cod_familia where i.tipo=2 order by cod_familia, name_indicador";
+                 " inner join geoserver.indicador i on i.cod_familia = f.cod_familia " \
+                 " left join geoserver.familia p on p.cod_familia = f.padre " \
+                 "where i.tipo=2 order by f.cod_familia, name_indicador";
 
         return self.query(sql).result()
 
